c3b3d2fb1077bc3aea69a28ca4d8d837b51be999
Joebias <joemarley1@gmail.com>
2022-08-16 01:15:55 +0100

    Converted it to firebase and now uses local unhosted json file for data for now

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Chefle from "./components/Chefle";
+import recipes from "./db.json";
 
 function App() {
   const [solution, setSolution] = useState("");
   const [instructions, setInstructions] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/recipes")
-      .then((res) => res.json())
-      .then((json) => {
-        const todaysRecipe = json[0];
-        setSolution(todaysRecipe.food);
-        setInstructions(todaysRecipe.instructions);
-      });
+    const todaysRecipe = recipes.recipes[0];
+    setSolution(todaysRecipe.food);
+    setInstructions(todaysRecipe.instructions);
   }, [setSolution]);
 
   return (
